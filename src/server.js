1c18f8ca8e18e64f273f40d68a891ebd58a7c70e
Denny <d3n1sf4z49@gmail.com>
2024-09-15 17:15:38 +0200

    step 3

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import students from './db/students.json';
+import pretty from 'pino-pretty';
+// import 'dotenv/config';
+// import students from './db/students.json';
 
 const setupServer = () => {
   const app = express();
   const PORT = Number(process.env.PORT) || 3000;
   const logger = pino({
     transport: {
-      target: pino - pretty,
+      target: 'pino-pretty',
     },
   });
 
   app.use(cors());
   app.use(logger);
-  app.use(express.json());
+  // app.use(express.json());
 
   app.use((req, res) => {
     res.status(404).json({
@@ -22,15 +24,15 @@ const setupServer = () => {
     });
   });
 
-  app.use((error, req, res) => {
-    res.status(500).json({
-      message: error.message,
-    });
-  });
+  // app.use((error, req, res) => {
+  //   res.status(500).json({
+  //     message: error.message,
+  //   });
+  // });
 
-  app.get('/students', (req, res) => {
-    res.json(students);
-  });
+  // app.get('/students', (req, res) => {
+  //   res.json(students);
+  // });
 
   app.listen(3000, () => console.log(`Server running on ${PORT}`));
 };
