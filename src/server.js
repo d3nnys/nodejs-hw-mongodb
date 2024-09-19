e5550bafab692c210bf994f0d9fe89077aef3da8
Denny <d3n1sf4z49@gmail.com>
2024-09-19 22:03:43 +0200

    step 1

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import 'dotenv/config';
-import * as contactServices from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
 
 const setupServer = () => {
   const app = express();
@@ -16,33 +16,7 @@ const setupServer = () => {
   app.use(cors());
   app.use(logger);
   app.use(express.json());
-
-  app.get('/contacts', async (req, res) => {
-    const data = await contactServices.getAllContacts();
-
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: data,
-    });
-  });
-
-  app.get('/contacts/:id', async (req, res) => {
-    const { id } = req.params;
-    const data = await contactServices.getContactById(id);
-
-    if (!data) {
-      return res.status(404).json({
-        message: 'Contact not found',
-      });
-    }
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${id}!`,
-      data: data,
-    });
-  });
+  app.use('/contacts', contactsRouter);
 
   app.use((req, res) => {
     res.status(404).json({
